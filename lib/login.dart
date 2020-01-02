1ce0bc4ca74c6dd1b5761eff32fe35f1461d22d4
Yevhenii Baranov <yevhenii.baranov@teamdev.com>
2020-01-02 23:28:41 +0200

    Add Google authentication to the project.

---
+++ lib/login.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class LoginWidget extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("Days off manager")),
+        body: AccountName());
+  }
+}
+
+class AccountName extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _AccountNameState();
+}
+
+class _AccountNameState extends State<AccountName> {
+  String username = "";
+  bool signedIn = false;
+
+  final GoogleSignIn signIn = GoogleSignIn(
+      scopes: ['email', 'https://www.googleapis.com/auth/contacts.readonly']
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Column(children: [
+          MaterialButton(
+            onPressed: () => doLogin(),
+            child: Text("Login with Google"),
+          ),
+          signedIn ? MaterialButton(
+            child: Text("Log out"),
+            onPressed: () => signOut(),
+          ) : Container(),
+          Text(username == "" ? "" : "Hello, $username")
+        ]));
+  }
+
+  doLogin() async {
+    var account = await signIn.signIn();
+
+    if (account != null) {
+      setState(() {
+        username = account.displayName;
+        signedIn = true;
+      });
+    }
+  }
+
+  signOut() async {
+    await signIn.signOut();
+    setState(() {
+      username = "";
+      signedIn = false;
+    });
+  }
+}
+
